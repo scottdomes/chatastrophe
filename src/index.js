b2a57265d5087143b8b7b9f699549a6f1a5e323d
YScott Domes <scott@musefind.com>
2017-08-15 18:34:42 -0700

    Added start script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(React.createElement('h1', false, 'Hello from React!'), document.getElementById('root'));
+ReactDOM.render(React.createElement('h1', false, 'Hello from Webpack!'), document.getElementById('root'));
