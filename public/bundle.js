35be43accc43e3bc3582e90699fe95ff92aad326
YScott Domes <scott@musefind.com>
2017-08-15 18:27:11 -0700

    Required bundle.js in index.html

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,17 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+console.log('hello from index.js!');
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-console.log('hello from index.js!');
+console.log('Hello from index2.js!');
 
 
 /***/ })
