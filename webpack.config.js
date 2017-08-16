27353f4f1b6b188af47263c2b5da2aa7c6e62d48
YScott Domes <scott@musefind.com>
2017-08-15 18:38:11 -0700

    Added Babel and configured loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,19 @@ module.exports = {
    path: __dirname + "/public",
    filename: "bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react'],
+          plugins: ['transform-class-properties']
+        }
+      }
+    ]
+  },
   devServer: {
     contentBase: "./public",
     historyApiFallback: true,
