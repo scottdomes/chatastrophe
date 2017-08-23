475ad72289d4b7866ebc50a6ad7e35c1080fde05
YScott Domes <scott@musefind.com>
2017-08-23 14:33:16 -0700

    Added form submission console log

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class LoginContainer extends Component {
-  state = { text: 'Hello from state!' };
+  state = { email: '', password: '' };
 
-  handleClick = () => {
-    this.setState({ text: 'State changed!' });
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
   };
 
   render() {
@@ -12,14 +21,22 @@ class LoginContainer extends Component {
       <div id="LoginContainer">
         <div id="Header">
           <img src="/assets/icon.png" alt="logo" />
-          <h1 onClick={this.handleClick}>
-            {this.state.text}
-          </h1>
+          <h1>Chatastrophe</h1>
         </div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>Sign in or sign up by entering your email and password.</p>
-          <input type="text" placeholder="Your email" />
-          <input type="password" placeholder="Your password" />
+          <input
+            type="text"
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+            placeholder="Your email"
+          />
+          <input
+            type="password"
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+            placeholder="Your password"
+          />
           <button className="red light" type="submit">
             Login
           </button>
