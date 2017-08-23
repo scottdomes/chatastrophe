9765f64f8aeb09cf103c70508ba206bba8405814
YScott Domes <scott@musefind.com>
2017-08-23 14:28:11 -0700

    Added state example

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class LoginContainer extends Component {
+  state = { text: 'Hello from state!' };
+
+  handleClick = () => {
+    this.setState({ text: 'State changed!' });
+  };
+
   render() {
     return (
       <div id="LoginContainer">
         <div id="Header">
           <img src="/assets/icon.png" alt="logo" />
-          <h1>Chatastrophe</h1>
+          <h1 onClick={this.handleClick}>
+            {this.state.text}
+          </h1>
         </div>
         <form>
           <p>Sign in or sign up by entering your email and password.</p>
