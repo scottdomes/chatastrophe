2bf50b9c7bbd0e0bb7bccce7629ca4cc3a4ac678
YScott Domes <scott@musefind.com>
2017-09-06 07:24:47 -0700

    Added redirect on login

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -22,12 +22,16 @@ class LoginContainer extends Component {
     }
   };
 
+  onLogin() {
+    this.props.history.push("/");
+  }
+
   login() {
     firebase
       .auth()
       .signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(res => {
-        console.log(res);
+        this.onLogin();
       })
       .catch(error => {
         if (error.code === 'auth/user-not-found') {
@@ -43,7 +47,7 @@ class LoginContainer extends Component {
       .auth()
       .createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then(res => {
-        console.log(res);
+        this.onLogin();
       })
       .catch(error => {
         console.log(error);
