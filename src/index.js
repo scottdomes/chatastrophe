ede53628ef3e5450548b0369b86b0f21992620d7
YScott Domes <scott@musefind.com>
2017-09-06 07:20:15 -0700

    Install react-router-dom and added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     const NextApp = require('./components/App').default;
     ReactDOM.render(
-     <App/>,
-     document.getElementById('root')
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      document.getElementById('root')
     );
   });
 }
