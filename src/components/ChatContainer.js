db38cf0e33efed8eacd44acdd55e2579fc049761
YScott Domes <scott@musefind.com>
2017-09-07 07:25:29 -0700

    Added logout button

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class ChatContainer extends Component {
+  handleLogout = () => {
+    firebase.auth().signOut();
+  };
+  
   render() {
     return (
       <div id="ChatContainer">
-        <Header />
+        <Header>
+          <button className="red" onClick={this.handleLogout}>
+            Logout
+          </button>
+        </Header>
         <h1>Hello from ChatContainer</h1>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div id="Header">
       <img src="/assets/icon.png" alt="logo" />
       <h1>Chatastrophe</h1>
+      {props.children}
     </div>
   );
 };
