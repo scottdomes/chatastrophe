bc4e2a0e379b4ebd16b1c7ad7f6f092f9bc8c6aa
YScott Domes <scott@musefind.com>
2017-09-08 06:52:33 -0700

    Added message-container and chat-input

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -8,13 +8,23 @@ export default class ChatContainer extends Component {
   
   render() {
     return (
-      <div id="ChatContainer">
+      <div id="ChatContainer" className="inner-container">
         <Header>
           <button className="red" onClick={this.handleLogout}>
             Logout
           </button>
         </Header>
-        <h1>Hello from ChatContainer</h1>
+        <div id="message-container">
+      
+        </div>
+        <div id="chat-input">
+          <textarea placeholder="Add your message..." />
+          <button>
+           <svg viewBox="0 0 24 24">
+             <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+           </svg>
+         </button>
+        </div>
       </div>
     );
   }
