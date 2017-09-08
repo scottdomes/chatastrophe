67dfdf4cdbb021ac6ef9e3e3514f10d497967f58
YScott Domes <scott@musefind.com>
2017-09-08 07:18:06 -0700

    Added sending and receiving messages from Firebase

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import UserContainer from './UserContainer';
 import './app.css';
 
 class App extends Component {
+  state = { user: null, messages: [] };
+
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
@@ -14,11 +16,34 @@ class App extends Component {
         this.props.history.push('/login');
       }
     });
+    firebase
+      .database()
+      .ref('/messages')
+      .on('value', snapshot => {
+        this.onMessage(snapshot);
+      });
   }
 
+  onMessage = snapshot => {
+    const messages = Object.keys(snapshot.val()).map(key => {
+      const msg = snapshot.val()[key];
+      msg.id = key;
+      return msg;
+    });
+    this.setState({ messages });
+  };
+
   handleSubmitMessage = msg => {
-    // Send to database
-    console.log(msg);
+    const data = {
+      msg,
+      author: this.state.user.email,
+      user_id: this.state.user.uid,
+      timestamp: Date.now()
+    };
+    firebase
+      .database()
+      .ref('messages/')
+      .push(data);
   };
 
   render() {
@@ -28,7 +53,12 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={() => <ChatContainer onSubmit={this.handleSubmitMessage} />}
+          render={() => (
+            <ChatContainer
+              onSubmit={this.handleSubmitMessage}
+              messages={this.state.messages}
+            />
+          )}
         />
         <Route path="/users/:id" component={UserContainer} />
       </div>
