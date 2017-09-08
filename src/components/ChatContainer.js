2bd645b161e47a49021dc6c25401579b5319c415
YScott Domes <scott@musefind.com>
2017-09-08 07:02:58 -0700

    Added change and keydown handlers to ChatContainer

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class ChatContainer extends Component {
+  state = { newMessage: '' };
+
   handleLogout = () => {
     firebase.auth().signOut();
   };
-  
+
+  handleInputChange = e => {
+    this.setState({ newMessage: e.target.value });
+  };
+
+  handleSubmit = () => {
+    this.props.onSubmit(this.state.newMessage);
+    this.setState({ newMessage: '' });
+  };
+
+  handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      this.handleSubmit();
+    }
+  };
+
   render() {
     return (
       <div id="ChatContainer" className="inner-container">
@@ -14,16 +32,19 @@ export default class ChatContainer extends Component {
             Logout
           </button>
         </Header>
-        <div id="message-container">
-      
-        </div>
+        <div id="message-container" />
         <div id="chat-input">
-          <textarea placeholder="Add your message..." />
-          <button>
-           <svg viewBox="0 0 24 24">
-             <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
-           </svg>
-         </button>
+          <textarea
+            placeholder="Add your message..."
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleKeyDown}
+            value={this.state.newMessage}
+          />
+          <button onClick={this.handleSubmit}>
+            <svg viewBox="0 0 24 24">
+              <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+            </svg>
+          </button>
         </div>
       </div>
     );
