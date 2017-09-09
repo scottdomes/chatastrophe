dd7f3b85539c97c0fbd2384f8a03b9c41c2ede9b
YScott Domes <scott@musefind.com>
2017-09-08 21:18:56 -0700

    Fleshed out UserContainer

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,16 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/users/:id" component={UserContainer} />
+        <Route
+          path="/users/:id"
+          render={({ history, match }) => (
+            <UserContainer
+              messages={this.state.messages}
+              messagesLoaded={this.state.messagesLoaded}
+              userID={match.params.id}
+            />
+          )}
+        />
       </div>
     );
   }
--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -3,17 +3,41 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 
 export default class UserContainer extends Component {
+  renderedUserEmail = false;
+
+  getAuthor = author => {
+    if (!this.renderedUserEmail) {
+      this.renderedUserEmail = true;
+      return <p className="author">{author}</p>;
+    }
+  };
+
   render() {
     return (
-      <div id="UserContainer">
+      <div id="UserContainer" className="inner-container">
         <Header>
           <Link to="/">
-            <button className="red">
-              Back To Chat
-            </button>
+            <button className="red">Back To Chat</button>
           </Link>
         </Header>
-        <h1>Hello from UserContainer for User {this.props.match.params.id}</h1>
+        {this.props.messagesLoaded ? (
+          <div id="message-container">
+            {this.props.messages.map(msg => {
+              if (msg.user_id === this.props.userID) {
+                return (
+                  <div key={msg.id} className="message">
+                    {this.getAuthor(msg.author)}
+                    <p>{msg.msg}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        ) : (
+          <div id="loading-container">
+            <img src="/assets/icon.png" alt="logo" id="loader" />
+          </div>
+        )}
       </div>
     );
   }
