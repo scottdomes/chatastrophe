750b639eae085b1d5a7c647dc55ffa5568bdf7a1
YScott Domes <scott@musefind.com>
2017-09-14 08:49:32 -0700

    Added cache invalidation and fetch interception

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,3 +23,25 @@ self.addEventListener('install', event => {
     })
   );
 });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    cache.keys().then(keyList => {
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
