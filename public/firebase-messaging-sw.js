679b3624c5f371296277171f8a5d267a108a6a3a
YScott Domes <scott@musefind.com>
2017-09-26 15:32:24 -0700

    Fixed messagingSenderId

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js'); 
 
 firebase.initializeApp({
-  'messagingSenderId': '85734589405'
+  'messagingSenderId': '120582509244'
 });
 
 console.log(firebase.messaging());
